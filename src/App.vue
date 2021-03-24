4fff3795925c8e3122df22bd4837e20f914274fd
anna olender <aolender@annas-MacBook-Pro.local>
2021-03-24 07:00:08 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header />
+    <label class="form__label">Name</label
+    >
+
+    <DialogContainer />
+
+
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import DialogContainer from './components/DialogContainer.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      name: '',
+      age: 0
+    }
+  },
+
   components: {
-    HelloWorld
+    HelloWorld,
+    Header,
+    DialogContainer
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>To do list</h1>
+    <p>This is your to do list. You can add here what you want to do</p>
+  </div>
+</template>
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+<style scoped>
+</style>
